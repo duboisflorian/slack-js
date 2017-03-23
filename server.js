9642bf714d14814cc015fcb24387b81ddeb4e1b7
duboisflorian <flo.dub305@gmail.com>
2017-03-23 16:51:01 +0100

    add serv

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
