ca01cebecf53d5784233d237f5a94b34ecbf7a7e
Eldii <m1105.108.baba@gmail.com>
2017-03-23 22:00:56 +0100

    Ajout de la base de données

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql      = require('mysql');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var connection = mysql.createConnection({
+  host     : 'mysql-slack-js.alwaysdata.net',
+  user     : 'slack-js',
+  password : 'yani123',
+  database : 'slack-js_project'
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,6 +30,16 @@ res.sendFile(__dirname + '/public/index.html');
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    var date = new Date();
+    var heure = date.getHours();
+    var minute = date.getMinutes();
+    var seconde = date.getSeconds();
+    var heurescomplete  = heure + ":" + minute + ":" + seconde;
+    connection.query("INSERT INTO message VALUES (NULL,'" + msg +" ', '" + heurescomplete +"')", function(err, rows, fields) {
+      if (err) throw err;
+      console.log('Message ajouté' + date);
+    });
+    console.log('Message envoye : ' + msg);
   });
 });
 
